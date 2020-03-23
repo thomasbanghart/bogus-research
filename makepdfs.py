b08aa67d6dbbb435152e1f86d5351dd30e69941d
TJ Banghart <thomas.banghart@looker.com>
2020-03-23 16:50:50 -0700

    Fixed errors with initial draft, uploads to GCS

--- makepdfs.py
+++ makepdfs.py
@@ -1,29 +1,36 @@
+from google.cloud import storage
 import sys
 import builtins as exceptions
 import looker_sdk 
+import os
 import time
 from typing import Dict, Optional
 
 sdk = looker_sdk.init40()
 models = looker_sdk.models40
 
-def swap_dataset(connection: str = "brick-layer", dataset: str="hubspot_marketing"):
+def swap_dataset(dataset: str="hubspot_marketing", connection: str = "brick-layer"):
     sdk.update_connection(connection, models.WriteDBConnection(database=dataset)) #Update connection reference 
-    return
+
+def download_dashboard(dataset: str, id: int):
+    child = os.fork() #Fork a process to run in the background
+    if child == 0:
+        run_render_task(dataset, id) #run render task as child 
+    else:
+        parent, status = os.waitpid(child, os.WNOHANG) #let parent exit 
     
-def download_dashboard(
-    # dashboard: models.Dashboard,
-    dashboard: id,
+def run_render_task(
+    dataset: str,
+    dashboard: str,
     style: str = "tiled",
     width: int = 612*2,
     height: int = 792*2,
     filters: Optional[Dict[str, str]] = None,
 ):
-    """Download specified dashboard as PDF"""
-    # assert dashboard.id
-    # id = int(dashboard.id)
+
+    #init render task
     task = sdk.create_dashboard_render_task(
-        id,
+        dashboard,
         "pdf",
         models.CreateDashboardRenderTask(
             dashboard_style=style,
@@ -33,31 +40,35 @@ def download_dashboard(
         height,
     )
 
+    #check if task could be created correctly 
     if not (task and task.id):
         raise exceptions.RenderTaskError(
-            f'Could not create a render task for "{dashboard.title}"'
-        )
+            f'Could not create a render task for "{pdf_name}"'
+    )
 
-    # poll the render task until it completes
-    elapsed = 0.0
+    #get pdf name and init GCS client
+    pdf_name = "{}-{}".format(dataset, id)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("brick-layer-testing")
+
+    elapsed = 0.0 # poll the render task until it completes
     delay = 0.5  # wait .5 seconds
     while True:
         poll = sdk.render_task(task.id)
         if poll.status == "failure":
             print(poll)
-            raise exceptions.RenderTaskError(f'Render failed for "{dashboard.title}"')
+            raise exceptions.RenderTaskError(f'Render failed for "{pdf_name}"')
         elif poll.status == "success":
             break
 
         time.sleep(delay)
         elapsed += delay
-    print(f"Render task completed in {elapsed} seconds")
 
-    result = sdk.render_task_results(task.id)
-    filename = f"{dashboard}.pdf"
-    with open(filename, "wb") as f:
-        f.write(result)
-    print(f'Dashboard pdf saved to "{filename}"')
+    result = sdk.render_task_results(task.id) #get render task results and set filename
+    filename = f"bogus_test/{pdf_name}.pdf"
 
-    return
-
+    
+    blob = bucket.blob(filename) #init GCS blob to write to
+    blob.upload_from_string(result, "application/pdf") 
+    
+    os._exit(0) #exit child process 
